929fec7614d2920712518f60f0633b52f0ac7c59
Nina <78531442+meowmeowmoore@users.noreply.github.com>
2021-07-06 21:37:33 +0500

    create class Search, writing async method searchRepositories

--- Main.js
+++ Main.js
@@ -1,10 +1,57 @@
-class CreateElement {
+class View {
     constructor() {
         this.app = document.getElementById('app');
-        this.input = document.createElement('input');
-        this.input.className = 'input-for-search';
+
+        this.searchLine = this.createElement('div', 'search-line')
+        this.input = this.createElement('input', 'input-for-search');
+        this.searchLine.append(this.input);
+
+        this.main = this.createElement('div', 'wrapper-repositories');
+        this.listOfRepositories = this.createElement('ul', 'list-repositories');
+        this.main.append(this.listOfRepositories);
+
+        this.app.append(this.searchLine);
+        this.app.append(this.main);
+
+    }
+
+    createElement (elementTag, elementClass) {
+        let element = document.createElement(elementTag);
+        if(elementClass) {
+            element.classList.add(elementClass);
+        }
+        return element;
+    }
+
+    // createRepository (repository) {
+    //     let elementRepository = this.createElement('li', 'element-repository');
+    //
+    //     this.listOfRepositories.append(elementRepository)
+    //
+    // } При событии добавления выбранного репозитория
+}
+
+class Search {
+    constructor(view) {
+        this.view = view;
+
+        this.view.input.addEventListener('keyup', this.searchRepositories.bind(this))
+
+
+    }
+
+    async searchRepositories() {
+        return await fetch(
+            `https://api.github.com/search/repositories?q=${this.view.input.value}`)
+            .then(res => {
+                if (res.ok) {
+                    return res.json()
+                } else {}
+            }).then(res => { console.log(res);
+                res.items.forEach(repository => console.log(repository)/*this.view.createRepository(repository)*/)
+            })
 
     }
 }
 
-new CreateElement();
+new Search(new View());
